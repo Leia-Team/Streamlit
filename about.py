0ac10d7b1779c024c7dcccf01e506c551b837752
nsarkhanov <nsarkhanov@gmail.com>
2021-10-14 19:03:30 +0200

    update

---
+++ about.py
@@ -0,0 +1,27 @@
+
+import streamlit as st
+import time
+import os
+
+
+def app():
+    st.title('About us ')
+    # for i in range(1, 10):
+    for i in range(1, 5):
+        st.balloons()
+        st.balloons()
+        time.sleep(2)
+        # st.balloons()
+        # st.balloons()
+        # st.balloons()
+        # st.balloons()
+        # st.balloons()
+        # st.balloons()
+        # time.sleep(3)
+        # st.balloons()
+        # st.balloons()
+        # st.balloons()
+        # st.balloons()
+        # st.balloons()
+        # st.balloons()
+        # time.sleep(1)
